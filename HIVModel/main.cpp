d380abe9fd621571c729d437708fc1d53264ae55
Mikaela Smit <mikaela.smit@gmail.com>
2015-06-05 16:00:11 +0100

    Initial Commit

---
+++ HIVModel/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HIVModel
+//
+//  Created by Mikaela Smit on 05/06/2015.
+//  Copyright (c) 2015 Mikaela Smit. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
